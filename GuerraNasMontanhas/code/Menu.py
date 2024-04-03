2c99c9aaa67f3b718c268649c345330d2937d013
Samuel Junio <samjunio.sj17@gmail.com>
2024-04-03 19:42:05 -0300

    Aula sobre criação e movimento de entidades

--- GuerraNasMontanhas/code/Menu.py
+++ GuerraNasMontanhas/code/Menu.py
@@ -17,10 +17,11 @@ class Menu:
         while True:
             self.window.blit(source=self.surf, dest=self.rect)
             self.menu_text(50, "Mountain", (255, 128, 0), ((WIN_WIDTH / 2), 70))
+            self.menu_text(50, "Shooter", (255, 128, 0), ((WIN_WIDTH / 2), 70))
             pygame.display.flip()
 
     def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
-        text_font: Font = pygame.font.SysFont(name="Lacida Sans Typewriter", size=text_size)
+        text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
         text_surf: Surface = text_font.render(text, True, text_color)
         text_rect: Rect = text_surf.get_rect(center=text_center_pos)
         self.window.blit(source=text_surf, dest=text_rect)
