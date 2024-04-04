1658b1fc2c13ebf6ff36f59370a0274f09f7fdeb
Samuel Junio <samjunio.sj17@gmail.com>
2024-04-03 21:40:37 -0300

    Aula sobre criação e movimento de entidades

--- GuerraNasMontanhas/code/Game.py
+++ GuerraNasMontanhas/code/Game.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
+import sys
+
 import pygame
 from pygame import Surface, Rect
 from pygame.font import Font
-from code.Const import WIN_WIDTH, WIN_HEIGHT
+from code.Const import WIN_WIDTH, WIN_HEIGHT, MENU_OPTION
+from code.Level import Level
 from code.Menu import Menu
 
 
@@ -12,13 +15,17 @@ class Game:
         pygame.init()
         self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
-
     def run(self):
-        pygame.mixer_music.load("./asset/Menu.wav")
-        pygame.mixer_music.play(-1)
-
         while True:
             menu = Menu(self.window)
-            menu.run()
+            menu_return = menu.run()
+
+            if menu_return in [MENU_OPTION[0], MENU_OPTION[1], MENU_OPTION[2]]:
+                level = Level(self.window, 'Level 1', menu_return)
+                level_return = level.run()
+            else:
+                pygame.quit()
+                sys.exit()
+
 
 
