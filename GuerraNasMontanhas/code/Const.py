d6d0c3d018a4870f95f8505cabfdf6d7fa4036d1
Samuel Junio <samjunio.sj17@gmail.com>
2024-04-04 21:11:36 -0300

    Aula sobre criação e movimento de entidades

--- GuerraNasMontanhas/code/Const.py
+++ GuerraNasMontanhas/code/Const.py
@@ -1,4 +1,6 @@
 # C
+import pygame
+
 COLOR_ORANGE = (255, 128, 0)
 COLOR_WHITE = (255, 255, 255)
 COLOR_YELLOW = (255, 255, 128)
@@ -13,6 +15,7 @@ WIN_WIDTH = 576
 WIN_HEIGHT = 324
 
 # E
+EVENT_ENEMY = pygame.USEREVENT + 1
 ENTITY_SPEED = {'Level1Bg0': 0,
                 'Level1Bg1': 1,
                 'Level1Bg2': 2,
@@ -20,4 +23,20 @@ ENTITY_SPEED = {'Level1Bg0': 0,
                 'Level1Bg4': 4,
                 'Level1Bg5': 5,
                 'Level1Bg6': 6,
+                'Player1': 4.7,
+                'Player2': 4.7,
+                'Enemy1': 3.0,
+                'Enemy2': 2
                 }
+# P
+PLAYER_K_UP = {'Player1': pygame.K_UP,
+               'Player2': pygame.K_w}
+
+PLAYER_K_DOWN = {'Player1': pygame.K_DOWN,
+                 'Player2': pygame.K_s}
+
+PLAYER_K_RIGHT = {'Player1': pygame.K_RIGHT,
+                  'Player2': pygame.K_d}
+
+PLAYER_K_LEFT = {'Player1': pygame.K_LEFT,
+                 'Player2': pygame.K_a}
--- GuerraNasMontanhas/code/Player.py
+++ GuerraNasMontanhas/code/Player.py
@@ -1,12 +1,26 @@
 #!/usr/bin/python
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
+import pygame
 
-from Entity import Entity
+from code.Const import ENTITY_SPEED, WIN_HEIGHT, WIN_WIDTH, PLAYER_K_UP, PLAYER_K_DOWN, PLAYER_K_RIGHT, PLAYER_K_LEFT
+from code.Entity import Entity
+
 
 class Player(Entity):
-    def __init__(self):
-        pass
+    def __init__(self, name: str, position: tuple):
+        super().__init__(name, position)
 
-    def move(self, ):
-        pass
+    def move(self):
+        pressed_key = pygame.key.get_pressed()
+        if pressed_key[PLAYER_K_UP[self.name]] and self.rect.top > 0:  # Se a tecla Seta para cima foi pressionada...
+            self.rect.centery -= ENTITY_SPEED[self.name]
+
+        if pressed_key[PLAYER_K_DOWN[self.name]] and self.rect.bottom < WIN_HEIGHT:
+            self.rect.centery += ENTITY_SPEED[self.name]
+
+        if pressed_key[PLAYER_K_RIGHT[self.name]] and self.rect.right < WIN_WIDTH:
+            self.rect.centerx += ENTITY_SPEED[self.name]
+
+        if pressed_key[PLAYER_K_LEFT[self.name]] and self.rect.left > 0:
+            self.rect.centerx -= ENTITY_SPEED[self.name]
 
